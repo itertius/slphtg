aa6941221314dcc05f9eeea4b7d7ab64e2cdd2a4
iterrius <85145267+itertius@users.noreply.github.com>
2024-09-07 11:45:41 +0700

    update

--- 12.py
+++ 12.py
@@ -1,5 +1,3 @@
-# not finished
-
 lst = []
 up = []
 while 1 :
@@ -10,4 +8,14 @@ while 1 :
     lst.append(temp.split(" "))
 
 print(lst)
+
+for i in range(len(lst)) :
+    for j in range(0, len(lst)-i-1) :
+        if int(lst[i][0])>int(lst[j+1][0]) :
+            temp = int(lst[i][0])
+            lst[i][0] = int(lst[j+1][0])
+            lst[j+1][0] = temp
+
+print(lst)
+
 print(up)
