def9dc31683dcb4836dae8d00e69ea6fbf2ff4ec
iterrius <85145267+itertius@users.noreply.github.com>
2024-09-07 12:27:53 +0700

    update 4.py

--- 4.py
+++ 4.py
@@ -2,5 +2,9 @@ arr = []
 for i in range(0, 5) :
     arr.append(int(input()))
 
-arr.sort()
+for i in range(len(arr)) :
+    for j in range(0, len(arr)-i-1) :
+        if arr[i]>arr[j+1] :
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+
 print(arr[2])
